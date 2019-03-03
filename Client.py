568493f628f7f9831367dac58586d09d166c91c9
Johan Jacobs <jacobs-johan@hotmail.com>
2019-03-03 15:16:06 +0100

    Updated Client.py file on desktop for slave interactions

--- Client.py
+++ Client.py
@@ -1,25 +1,41 @@
-import socket
 import SharedFunctions as sf
+import threading
+
+GNUThread = threading.Event()
+AoA = 90
 
 
+def runGNURadio():
+    print("running gnu radio")
+    #AoA should be updated in here somewhere
 
 def main():
     print("Starting up client")
 
-    angle = 123
-    angle_b = angle.to_bytes(4, byteorder='big')
-
     # Create a client socket at the same IP and port as receiver in order to send to the server
-    serverIP = '127.0.0.1'
+    # serverIP = '127.0.0.1'
+    serverIP = '192.168.0.128'
     serverPort = 5000
 
-    data = None
-    while (data != 'Shut down'):
-        data = str(input("What is your message? \n--> "))
-        data_b = data.encode('utf-8')
-        clientSock = sf.createSocket(serverIP, serverPort, serverBool=False)
-        clientSock.sendall(data_b)
+    running = True
+    while running:
+        #data = str(input("What is your message? \n--> "))
+        #data_b = data.encode('utf-8')
+        s = sf.createSocket(serverIP, serverPort, serverBool=False)
 
+        # After connection is established, wait for input from Server
+        task = s.recv(1024)
+        print("New task is: ", task)
+        if (task == b'GNU'):
+            if not(GNUThread.is_set()):
+                threading.Thread(target = runGNURadio).start()
+                GNUThread.set()
+        elif (task == b'AoA'):
+            s.sendall(AoA.to_bytes(1, byteorder='big'))
+        elif (task == b'Shut down'):
+            running = False
+        else:
+            print("Got unknown command: ", task)
     #clientSock.close() #not needed apparently
 
 
