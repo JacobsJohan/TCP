f930526c5449e6ad9a07a43c84133a3441b6d4fb
Johan Jacobs <jacobs-johan@hotmail.com>
2019-05-27 10:18:19 +0200

    Update 2019/05/27

--- Client2/SharedFunctions.py
+++ Client2/SharedFunctions.py
@@ -1,8 +1,6 @@
 import socket
 
-
-
-# Create a socket for either a client or a server. If serverBool is True,
+# Create a socket for either a client or a server.
 def createSocket(ip, port, serverBool=None):
     if (serverBool is None):
         serverBool = False
@@ -14,13 +12,3 @@ def createSocket(ip, port, serverBool=None):
         s.connect((ip, port))
     #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     return s
-
-def rcvAll(c):
-    data = c.recv(1024)
-    maxlen = 255 # We can only send 256 dwords, but header up to message length already contains a dword
-    msglen_b = data[2].to_bytes(1, byteorder='big') + data[3].to_bytes(1, byteorder='big')
-    msglen = int.from_bytes(msglen_b, byteorder='big')
-    while (msglen> maxlen):
-        data = data + c.recv(1024)
-        msglen = msglen - maxlen
-    return data
